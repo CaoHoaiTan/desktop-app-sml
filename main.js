e0ec3a3c253eb3eabd161d78ee15c3b4c95cba6c
Cao Hoài Tấn <caohoaitan@gmail.com>
2023-05-10 23:08:32 +0700

    1 screeen

--- main.js
+++ main.js
@@ -1,43 +1,60 @@
-// Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain } = require("electron");
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
+function createWindow() {
+  const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      nodeIntegration: true
     }
-  })
+  });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  win.loadFile('index.html');
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// Function to create child window of parent one
+function createChildWindow() {
+  childWindow = new BrowserWindow({
+    width: 700,
+    height: 700,
+    modal: true,
+    show: false,
+    parent: mainWindow, // Make sure to add parent window here
+  
+    // Make sure to add webPreferences with below configuration
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+  });
+  
+  // Child window loads settings.html file
+  childWindow.loadFile("register.html");
+  
+  childWindow.once("ready-to-show", () => {
+    childWindow.show();
+  });
+}
+
+
+ipcMain.on("openChildWindow", (event, arg) => {
+  console.log("hello");
+  createChildWindow();
+});
+  
 app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+  createWindow();
+  
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+  
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
